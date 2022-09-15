bf1551e29f40d90a75f15a8cbfb89d379b8723c7
AndreyZaycev <zaycev.a@tiv.ru>
2022-09-15 15:53:29 +0300

    Выполнил домашнее задание к уроку № 2

---
+++ HomeWorks/BackgroundInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    //общий класс для всего курса : класс общей информации
+    internal class BackgroundInformation
+    {
+        public static void GetBackgroundInfo()
+        {
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Курс :          Основы ООП в С#");
+            Console.WriteLine("Преподаватель : Кучма Андрей Витальевич");
+            Console.WriteLine("Студент :       Зайцев Андрей");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HomeWorks/ILessons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    //общий интерфейс для всего курса
+    public interface ILessons
+    {
+        //номер домашнего задания
+        string Number { get; }
+
+        //описание домашнего задания
+        string Description { get; }
+
+        //запуск задания
+        void Run();
+    }
+}
